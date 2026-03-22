11fff633641934f50be787a5062148c281027a43
Eber <www.eberacunaquispe@gmail.com>
2026-03-22 10:51:01 -0500

    Se elimino el archivo

--- src/about.js
+++
@@ -1,6 +0,0 @@
-export function loadAbout() {
-    const container = document.getElementById("content");
-    const title = document.createElement("h2");
-    title.textContent = "About";
-    container.appendChild(title);
-}
